0fd2f290c9592512dd1be86a16a9570b0d46e6ef
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-04-12 12:42:26 +0200

    Reading width for audio-waveform from video now (fixes issue #4)

--- config/scripts/audio_wave.lua
+++ config/scripts/audio_wave.lua
@@ -1,10 +1,12 @@
 mp.register_event('file-loaded', function()
     local count = 0
 
-    -- Show audio waveform underneith the video:
-    mp.set_property('lavfi-complex', '[aid1]asplit[ao][a1],[a1]showwaves=size=720x120:rate=25:mode=line:colors=white:scale=sqrt:split_channels=1[vida],[vid1][vida]vstack=inputs=2[vo]')
+    -- Set resolution of `showwaves` filter:
+    local width = mp.get_property("width")
+    local resolution = width .. 'x180'
 
-    --{filter = {'[aid1]asplit[ao][a1],[a1]showwaves=size=720x120:rate=25:mode=line:colors=white:scale=sqrt:split_channels=1[vida],[vid1][vida]vstack=inputs=2[vo]'}}, --0 audio waveform
+    -- Show audio waveform underneith the video:
+    mp.set_property('lavfi-complex', '[aid1]asplit[ao][a1],[a1]showwaves=size='..resolution..':rate=25:mode=line:colors=white:scale=sqrt:split_channels=1[vida],[vid1][vida]vstack=inputs=2[vo]')
 end)
 
 mp.register_event('end-file', function ()
