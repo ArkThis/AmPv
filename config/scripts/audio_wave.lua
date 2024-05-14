a4f735c4d8ba8778486eee9c773a969b7dcc63eb
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2024-05-14 16:19:38 +0200

    Added vu-meter overlay and split lavfi filters in blocks

--- config/scripts/audio_wave.lua
+++ config/scripts/audio_wave.lua
@@ -1,12 +1,22 @@
-mp.register_event('file-loaded', function()
-    local count = 0
+-- Individual audio filter building blocks:
+local f_asplit2 = "[aid${audio_track}]asplit[ao][a1]"
+local f_asplit3 = "[aid${audio_track}]asplit=3[ao][a1][a2]"
+local f_showwaves = "[a1]showwaves=size=${video_width}x180:rate=25:mode=line:colors=white:scale=sqrt:split_channels=1[v1]"
 
-    -- Set resolution of `showwaves` filter:
-    local width = mp.get_property("width")
-    local resolution = width .. 'x180'
+local f_showvolume = "[a2]showvolume=c=CHANNEL:p=0.5:dm=1:w=${video_width}[v2]"
+local f_stack1 = "[vid1][v1]vstack=inputs=2[vidb]"
+local f_overlay1 = "[vidb][v2]overlay[vo]"
+
+
+mp.register_event('file-loaded', function()
+    local audio_track = 1
+    local video_width = mp.get_property("width")
+
+    local waveform_filter = f_asplit3 ..','.. f_showwaves ..','.. f_showvolume ..','.. f_stack1 ..','.. f_overlay1
+    local filter = waveform_filter:gsub("${audio_track}", audio_track):gsub("${video_width}", video_width) 
 
     -- Show audio waveform underneith the video:
-    mp.set_property('lavfi-complex', '[aid1]asplit[ao][a1],[a1]showwaves=size='..resolution..':rate=25:mode=line:colors=white:scale=sqrt:split_channels=1[vida],[vid1][vida]vstack=inputs=2[vo]')
+    mp.set_property('lavfi-complex', filter)
 end)
 
 mp.register_event('end-file', function ()
